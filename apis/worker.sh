e93f0a553289d8eb21e02b89120ac0daf5267c71
折戟沉沙、丿 <36452456+bincooo@users.noreply.github.com>
2024-05-15 09:30:40 +0800

    Update worker.sh

--- apis/worker.sh
+++ apis/worker.sh
@@ -3,8 +3,8 @@ cd /app/main
 pnpm install
 nohup pnpm run dev > out.log 2>&1 &
 
-# cd /app
-# nohup ./free-gpt3.5-2api > gpt35.log 2>&1 &
+cd /app
+nohup ./free-gpt3.5-2api > gpt35.log 2>&1 &
 
 cd /app/firecrawl/apps/api
 pnpm install && pnpm run build
