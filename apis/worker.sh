413fef45ccdae004297a9e835609490040850a00
bincooo <bingco.zn@gmail.com>
2024-06-22 00:43:40 +0800

    udpate apis

--- apis/worker.sh
+++ apis/worker.sh
@@ -1,12 +1,15 @@
-git clone https://github.com/bincooo/worker-laf.git /app/main
-cd /app/main
-pnpm install
-nohup pnpm run dev > out.log 2>&1 &
-
-cd /app
-nohup ./free-gpt3.5-2api > gpt35.log 2>&1 &
+#!/bin/sh
+cd /app/ppl
+nohup python3 proxyPool.py schedule > /app/out.log 2>&1 &
+nohup python3 proxyPool.py server   > /app/out.log 2>&1 &
 
 cd /app/firecrawl/apps/api
 pnpm install && pnpm run build
-nohup pnpm run start:production  > out.log 2>&1 &
-nohup pnpm run worker:production > out.log 2>&1 &
+nohup pnpm run start:production  > /app/out.log 2>&1 &
+nohup pnpm run worker:production > /app/out.log 2>&1 &
+
+
+git clone https://github.com/bincooo/worker-laf.git /app/main
+cd /app/main
+pnpm install
+pnpm dev
